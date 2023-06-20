86d31125bebab6ab44e2f3099203f63c10784843
makersmarx <bbiehn1@gmail.com>
2023-06-20 19:06:25 -0400

    add loop to search by class name test

--- scraper.py
+++ scraper.py
@@ -7,7 +7,13 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find("div")
+results = soup.find_all("div")
+logos = soup.find(class_="Footer__LinkedLogo-sc-1mmo66d-9 gxZSWc")
 
-with open("test.html", "w") as file:
-    file.write(results.prettify())
+for i in results:
+    if logos in i:
+        print("I AM HERE" + str(i))
+
+
+# with open("test.html", "w") as file:
+#     file.write(results.prettify())
