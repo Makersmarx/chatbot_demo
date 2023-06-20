65d2050a7d2b140efeb0ea35432e7075e9252642
makersmarx <bbiehn1@gmail.com>
2023-06-20 19:48:22 -0400

    add loop to scrap only svg test

--- scraper.py
+++ scraper.py
@@ -6,13 +6,23 @@ URL = "https://movableink.com/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+# image_ = soup.find_all("div")
 
-results = soup.find_all("div")
-logos = soup.find(class_="Footer__LinkedLogo-sc-1mmo66d-9 gxZSWc")
+image_ = soup.find_all("svg")
 
-for i in results:
-    if logos in i:
-        print("I AM HERE" + str(i))
+for index, i in enumerate(image_):
+    with open(f"image_{index}.svg", "w") as f:
+        f.write(str(i))
+
+# results = soup.find_all("div")
+# logos = soup.find(class_="Footer__LinkedLogo-sc-1mmo66d-9 gxZSWc")
+
+# for i in results:
+#     if logos in i:
+#         with open("test.html", "w") as file:
+#             file.write(i.prettify())
+#     else:
+#         print("Cant Find It")
 
 
 # with open("test.html", "w") as file:
