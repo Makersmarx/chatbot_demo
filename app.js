4c17bb5e2a1dc437d4034cf5787daba44318ca50
makersmarx <bbiehn1@gmail.com>
2023-06-23 17:55:29 -0400

    test bard-ai

---
+++ app.js
@@ -0,0 +1,20 @@
+// import Bard, { askAI } from 'bard-ai';
+
+// const COOKIE_KEY =
+//   'Need Personal Cookie labeled __Secure-1PSID';
+
+// await Bard.init(COOKIE_KEY);
+
+// console.log(await askAI('is attentive a partner of movable ink?'));
+
+// import express from 'express';
+// const app = express();
+// const port = 8080;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`(¬ ºཀ´°)¬ eats port ${port}`);
+// });
--- scraper.py
+++ scraper.py
@@ -6,13 +6,7 @@ URL = "https://movableink.com/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-# image_ = soup.find_all("div")
-
-image_ = soup.find_all("svg")
-
-for index, i in enumerate(image_):
-    with open(f"image_{index}.svg", "w") as f:
-        f.write(str(i))
+image_ = soup.find_all("div")
 
 # results = soup.find_all("div")
 # logos = soup.find(class_="Footer__LinkedLogo-sc-1mmo66d-9 gxZSWc")
@@ -25,5 +19,5 @@ for index, i in enumerate(image_):
 #         print("Cant Find It")
 
 
-# with open("test.html", "w") as file:
-#     file.write(results.prettify())
+with open("test.html", "w") as file:
+    file.write(soup.prettify())
