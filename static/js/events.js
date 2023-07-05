800cf054b7452b92c884ebdb68c0a5f337bc12a5
makersmarx <bbiehn1@gmail.com>
2023-07-04 21:03:10 -0400

    add geolocation and convert lat & lon to city zip. Show sample in grid block one

--- static/js/events.js
+++ static/js/events.js
@@ -3,10 +3,27 @@
 const form = document.querySelector('.input-form');
 const message = document.querySelector('.messages');
 
-// if (message.textContent.length < 20) {
-//   message.textContent =
-//     'Hello and welcome to our Matt bot. Feel free to ask me about the ways Movable ink can help your company';
-// }
-
 const text = message.textContent;
-console.log(text);
+
+// test and grab users location, ping free api to convert lat/lon
+const gridOne = document.querySelector('.gridOne');
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    gridOne.innerHTML = 'Geolocation is not supported by this browser.';
+  }
+};
+
+// Show users city/zipcode in first grid block
+const showPosition = async (position) => {
+  const response = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=en`
+  );
+  const places = await response.json();
+  gridOne.innerHTML =
+    'City: ' + places.locality + '<br>Zipcode: ' + places.postcode;
+};
+
+getLocation();
